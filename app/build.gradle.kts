9fef0bd9f1c88a8bff76356b877699a24e5021c1
Nicat Zeynalli <nicat21308@gmail.com>
2024-04-21 14:42:12 +0400

    fix in the landing page and navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.glide)
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/movieapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.movieapp.ui.activity
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.movieapp.R
 import com.example.movieapp.databinding.ActivityMainBinding
 
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
     }
